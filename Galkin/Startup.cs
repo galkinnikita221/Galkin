1bae753a5ad29b8c21553daf8556696be0c7d7e4
Ann <iliuvv@mail.ru>
2024-06-22 09:55:25 +0500

    Разработана страница StartUp, Galkin

--- Galkin/Startup.cs
+++ Galkin/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,12 +15,44 @@ namespace Galkin
     {
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Авторизация пользователей",
+                    Description = "Авторизация пользователей"
+                });
+                c.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v2",
+                    Title = "Фильмы в кинотеатре",
+                    Description = "Добавление, удаление, изменение и получение списка о кинотеатрах"
+                });
+                c.SwaggerDoc("v3", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v3",
+                    Title = "Авторизация пользователей",
+                    Description = "Добавление, удаление, изменение и получение списка о фильмах"
+                });
+                string filePath = Path.Combine(AppContext.BaseDirectory, "Galkin.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+            app.UseMvcWithDefaultRoute();
+            app.UseStatusCodePages();
+            app.UseSwagger();
+            app.UseDeveloperExceptionPage();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Авторизация");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "Кинотеатры");
+                c.SwaggerEndpoint("/swagger/v3/swagger.json", "Фильмы");
+            });
         }
     }
 }

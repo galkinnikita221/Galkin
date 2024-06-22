c833e78552fbe6325241f738f856b8965505eceb
Ann <iliuvv@mail.ru>
2024-06-22 09:58:24 +0500

    Разработанные модели. Galkin

---
+++ Galkin/Model/Cinema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Galkin.Model
+{
+    public class Cinema
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string time { get; set; }
+        public int price { get; set; }
+        public string vozm { get; set; }
+    }
+}
